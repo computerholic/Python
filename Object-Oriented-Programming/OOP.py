508534b39efa9a7eed4fe33b55f1c0c4c60f074a
Seyed Mahdi Kashani <mahdi.kashanipour@gmail.com>
2018-07-06 12:17:45 -0400

    https://www.youtube.com/watch?v=ZDa-Z5JzLYM
    5:14

---
+++ Object-Oriented-Programming/OOP.py
@@ -0,0 +1,28 @@
+print("========== God ========= \n\n\n")
+
+class Employee:
+	pass
+
+emp_1 = Employee()
+emp_2 = Employee()
+
+
+print(emp_1)
+print(emp_2)
+
+emp_1.first = 'Coery'
+emp_1.last = 'Schafer'
+emp_1.email = 'Corey.Schafer@company.com'
+emp_1.pay = 50000
+
+# %%
+
+emp_2.first = 'Test'
+emp_2.last = 'User'
+emp_2.email = 'Test.User@company.com'
+emp_2.pay = 60000
+
+print("Help\n")
+
+
+#---------------
