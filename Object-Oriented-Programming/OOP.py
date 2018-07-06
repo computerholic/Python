a80cf14c08dac38df5c1ff0e90de1f414b8487e7
Seyed Mahdi Kashani <mahdi.kashanipour@gmail.com>
2018-07-06 12:31:32 -0400

    9:49
    https://www.youtube.com/watch?v=ZDa-Z5JzLYM

--- Object-Oriented-Programming/OOP.py
+++ Object-Oriented-Programming/OOP.py
@@ -1,28 +1,24 @@
 print("========== God ========= \n\n\n")
 
 class Employee:
-	pass
+	
+	def __init__(self, first, last, pay):  #Constuction. #  Init method - Initialize. #Self is an instance.
+		self.first = first
+		self.last = last
+		self.pay = pay
+		self.email = first + '.' + last + '@company.com'
 
-emp_1 = Employee()
-emp_2 = Employee()
+
+emp_0 = Employee('Mahdi','Kashani','130000')
+emp_1 = Employee('Ben','Taylor','150000')
+emp_2 = Employee('Maria Clare','Nunley','90000')
 
 
-print(emp_1)
-print(emp_2)
-
-emp_1.first = 'Coery'
-emp_1.last = 'Schafer'
-emp_1.email = 'Corey.Schafer@company.com'
-emp_1.pay = 50000
-
-# %%
-
-emp_2.first = 'Test'
-emp_2.last = 'User'
-emp_2.email = 'Test.User@company.com'
-emp_2.pay = 60000
+print(emp_1.email)
+print(emp_2.email)
 
 print("Help\n")
 
+print(emp_0.first,emp_1.first,emp_2.first)
 
-#---------------
+print('{} {}'.format(emp_0.first,emp_0.last))
