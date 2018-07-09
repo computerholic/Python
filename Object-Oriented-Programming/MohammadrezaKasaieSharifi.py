01914877042ad7f0ec076525a07aa99916ed867a
Seyed Mahdi Kashani <mahdi.kashanipour@gmail.com>
2018-07-08 23:53:16 -0400

    Tarife tavabe rooye class. 23:53:10.
    Differences in naming the methods and Classes.

--- Object-Oriented-Programming/MohammadrezaKasaieSharifi.py
+++ Object-Oriented-Programming/MohammadrezaKasaieSharifi.py
@@ -3,14 +3,15 @@ class ClassName:  # Always ClassName! All start with Capital.
 	author: Mahdi.
 	email: mahdi@uga.edu.
 	'''
-	pass
+	
+	def thisFunctionName(self, name):  # Always pass the self as an input.
+		print(name)  # Har tabe besoorate pishfarz None ro bar migardoone.
 
 if __name__ == '__main__':  # Module besurate dasti ejra shode.
 	obj1 = ClassName()  # Making an instance from an object.
-	print(obj1.__doc__)
+	print(obj1.thisFunctionName('this is my name.'))  # Baraye function ee ke roye sheye object1 ejra mishe . mizarim.	
 
-# qpydoc is very important to see the documentaion of each class.
 
-# print(help(int))
-
-# Now we are interested to know how to define a method in a class. July 8, 21:03:40
+# Note 2: har function ruye yek object ejra mishe!
+# Class avale har kalame bayad bozorg bashe! for function this is different:
+# thisFunctionName(self).
