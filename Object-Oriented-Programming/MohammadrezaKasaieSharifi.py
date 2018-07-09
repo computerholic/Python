df870d273919d104ecf0f6118a06456238aaf80c
Seyed Mahdi Kashani <mahdi.kashanipour@gmail.com>
2018-07-09 00:18:58 -0400

    Constructive Method is done
    Next one is Class variable.

--- Object-Oriented-Programming/MohammadrezaKasaieSharifi.py
+++ Object-Oriented-Programming/MohammadrezaKasaieSharifi.py
@@ -4,23 +4,28 @@
 from datetime import datetime  # Ino man import mikonam ta betoonam ba object datetime kar konam.
 # Onject zaman ro mikham azash estefade konam.
 
-class ClassName:
+class Person:
 	'''
 	author: Mahdi.
 	email: mahdi@uga.edu.
 	'''
 
 	# Different methods have different names due to their responsibilities. One of them is constructor which we make an object that method happens!
-	def __init__(self):  # Initialize.  # This is the constructor method. and as usual we should pass it the self.
-		self.created = datetime.now()
-		self.birth =
-		
-		print('Object is created')
+	def __init__(self, name):  # Initialize.  # This is the constructor method. and as usual we should pass it the self.
+		self.name = name
+		self.birth = datetime.now()
+		print('Human is created on',str(self.birth))
 
 	def thisFunctionName(self, name):
 		print(name)  # Har tabe besoorate pishfarz None ro bar migardoone.
 
 if __name__ == '__main__':  # Module besurate dasti ejra shode.
-	obj1 = ClassName()  # Making an instance from an object.
+	obj1 = Person('Mahdi')  # Making an instance from an object.
 
-print(datetime.now())
+# ----------------------- Attributes -----------------------------------
+# print(dir(obj))  # We can see which attributes it have. 
+
+# How can I add attribute to a Class?
+# obj.name = 'Ali'
+# dir(obj)
+# So I can add attribute to it.
