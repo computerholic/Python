e74fc1e142ee1e5a94e0ef3ff5ca98dd0d197da8
Seyed Mahdi Kashani <mahdi.kashanipour@gmail.com>
2018-07-09 00:09:33 -0400

    00:09:31

--- Object-Oriented-Programming/MohammadrezaKasaieSharifi.py
+++ Object-Oriented-Programming/MohammadrezaKasaieSharifi.py
@@ -1,15 +1,26 @@
+# Karbordhaye method constructor?
+# -------------------------------
+
+from datetime import datetime  # Ino man import mikonam ta betoonam ba object datetime kar konam.
+# Onject zaman ro mikham azash estefade konam.
+
 class ClassName:
 	'''
 	author: Mahdi.
 	email: mahdi@uga.edu.
 	'''
+
 	# Different methods have different names due to their responsibilities. One of them is constructor which we make an object that method happens!
-	def __init__(self):
+	def __init__(self):  # Initialize.  # This is the constructor method. and as usual we should pass it the self.
+		self.created = datetime.now()
+		self.birth =
+		
 		print('Object is created')
-	
-	
+
 	def thisFunctionName(self, name):
 		print(name)  # Har tabe besoorate pishfarz None ro bar migardoone.
 
 if __name__ == '__main__':  # Module besurate dasti ejra shode.
 	obj1 = ClassName()  # Making an instance from an object.
+
+print(datetime.now())
