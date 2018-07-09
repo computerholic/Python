091a26f103a072d3c91325756e8be040feaaa2e9
Seyed Mahdi Kashani <mahdi.kashanipour@gmail.com>
2018-07-08 21:05:18 -0400

    How to make a method in a class - Kasaee
    pydoc - help and ...

--- Object-Oriented-Programming/MohammadrezaKasaieSharifi.py
+++ Object-Oriented-Programming/MohammadrezaKasaieSharifi.py
@@ -1,8 +1,16 @@
-class Classname:  # Always ClassName! All start with Capital.
+class ClassName:  # Always ClassName! All start with Capital.
+	'''
+	author: Mahdi.
+	email: mahdi@uga.edu.
+	'''
 	pass
 
 if __name__ == '__main__':  # Module besurate dasti ejra shode.
 	obj1 = ClassName()  # Making an instance from an object.
-	print(type(obj1))
+	print(obj1.__doc__)
 
-# pydoc is very important to see the documentaion of each class.
+# qpydoc is very important to see the documentaion of each class.
+
+# print(help(int))
+
+# Now we are interested to know how to define a method in a class. July 8, 21:03:40
