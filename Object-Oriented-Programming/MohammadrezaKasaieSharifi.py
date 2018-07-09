cb1643d20124c1343f55bb9059196ce448c05226
Seyed Mahdi Kashani <mahdi.kashanipour@gmail.com>
2018-07-09 03:22:00 -0400

    __del__ for each method.
    Very important to know.
    
    The next functions are : is subclass and ...

--- Object-Oriented-Programming/MohammadrezaKasaieSharifi.py
+++ Object-Oriented-Programming/MohammadrezaKasaieSharifi.py
@@ -1,12 +1,14 @@
-# Karbordhaye method constructor?
-# -------------------------------
+# Soal? Which __init__ ejra mishe?
+# Overwrite kardan ya sarbar gozari?
+# ----------------------------------------------------------------------
 
+# Aval mibine Class __init__ dare ya na! Age dasht uno enjra mikone. Age nadasht mire
+# unyekio negah mikone.
 from datetime import datetime
 
 class Test:
 	test = 'Some string just to show that Class Test is working.'
-	
-	
+
 class Person:
 	''' __doc__
 	author: Mahdi.
@@ -17,37 +19,39 @@ class Person:
 	def __init__(self, name):  
 		self.name = name
 		self.birth = datetime.now()
+		print('He was a Person.')
 
 		print('\n'+name+' is created on '+ str(self.birth),'.\n')
 		Person.thisCount += 1
 		
 	def thisFunctionName(self, name):
-		print(name)
+		print(name, 'Called from class of Person.')
 		
 	def __del__(self):
-		print(self.name,' destroyed')
+		print(self.name,'destroyed')
+		print('He was a Person.')
+		
 		
 class ItMan(Person, Test):  # In az ki bayad ers bari kone ? Az Person! # Hamamun esm darimo. Tarikh tavalod darimo ...
 	
 	def __init__(self, name, email):
 		self.name = name
 		self.email = email
-	
-	
+		print('He was an IT man :(.')
+	def __del__(self):
+		print(self.name,'destroyed.')
+		print('He was an IT man :(.')
 
 if __name__ == '__main__':
 	
-	
 	jadi = ItMan('Jadi', 'jadijadi@gmail.com')
-	
 	
 	print('jadi.name:', jadi.name)
 	print('jadi.email:',jadi.email)
 	print('jadi.test:',jadi.test)
 	print(Test.__bases__)  # in neshoon mide in class az kodum kelasa dare ers bari mikone. # Inheritance.
-
-
-
+	
+	Mahdi = Person('Mahdi')
 
 #	print('jadi.birth:', jadi.birth)
 	
