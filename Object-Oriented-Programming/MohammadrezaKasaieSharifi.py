5cd5257bec163cdee5b00f7d4fb87d23e819f426
Seyed Mahdi Kashani <mahdi.kashanipour@gmail.com>
2018-07-09 03:04:18 -0400

    3:03 After radio geek + django.
    I love django - database and ...

--- Object-Oriented-Programming/MohammadrezaKasaieSharifi.py
+++ Object-Oriented-Programming/MohammadrezaKasaieSharifi.py
@@ -3,15 +3,17 @@
 
 from datetime import datetime
 
+class Test:
+	
 class Person:
 	''' __doc__
 	author: Mahdi.
 	email: mahdi@uga.edu.
 	'''
-	thisCount = 0;  # This is a Class variable.
+	thisCount = 0;
 
 	def __init__(self, name):  
-		self.name = name  # This is an instance variable.
+		self.name = name
 		self.birth = datetime.now()
 
 		print('\n'+name+' is created on '+ str(self.birth),'.\n')
@@ -22,37 +24,25 @@ class Person:
 		
 	def __del__(self):
 		print(self.name,' destroyed')
-
-if __name__ == '__main__':  # Module besurate dasti ejra shode.
+		
+class ItMan(Person):  # In az ki bayad ers bari kone ? Az Person! # Hamamun esm darimo. Tarikh tavalod darimo ...
 	
-	Mahdi = Person('Mahdi')
-	print('Mahdi.birth:', Mahdi.birth)
+	def __init__(self, name, email):
+		self.name = name
+		self.email = email
 	
-##	2 Attribute ee ke har object dare:
-
-##	__doc__
-##   In hamun tozihate dakhele __doc__ hast.
-
-##   obj.__module__
-##	__main__
-
-#	print('Mahdi.__module__:', Mahdi.__module__,'\n')
 	
-#	print('Mahdi.__dict__:', Mahdi.__dict__,'\n')
 
-## __name__ ru kelas tarif mishe na object.
-#	print("Person.__name__:",Person.__name__)
+if __name__ == '__main__':
 	
-## __bases__
-#	print("Person.__bases__:", Person.__bases__)
 	
-## __ Garbage Collection ==> Happening by python. --> we can define a method name Distructor. It can show you a message when the object is removing.
+	jadi = ItMan('Jadi', 'jadijadi@gmail.com')
+	
+	
+	print('jadi.name:', jadi.name)
+	print('jadi.email:',jadi.email)
 
-# a = 1
-# b = a
-# id(a)
-# id(b)  # Same as a. Now we have two different variables which are pointing to the same amount.
-# del(a)
-# We do not have the arrow from a to 1 any more.
-# If I say del(b) there is no a and b.
-# Now python will figure out that we have an object with no use!
+#	print('jadi.birth:', jadi.birth)
+	
+#  To python mige harchandta mikhay Class dashte bash. Object mitune az chandta ers bebare!
+#  Bazi zabana ye Class bozorg tush darim bad umade tike tikash karde. :-)
