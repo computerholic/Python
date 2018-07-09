c52a5307d035bcf16c71fc55f60e85395f7bcace
Seyed Mahdi Kashani <mahdi.kashanipour@gmail.com>
2018-07-09 03:56:37 -0400

    Done- MohammadSharifi- Syntax Python - 3:57 AM

--- Object-Oriented-Programming/MohammadrezaKasaieSharifi.py
+++ Object-Oriented-Programming/MohammadrezaKasaieSharifi.py
@@ -1,7 +1,9 @@
 # Private ? Public ? and hidden sazi chetoriast?
 
 from datetime import datetime
+from functools import total_ordering  # age esmesho ghable esme class benevisam va @ pass bedam in ye decoratore! yani age yekio neveshti baghiaro ham miare.
 
+@total_ordering
 class Test:
 	test = 'Some string just to show that Class Test is working.'
 
@@ -12,38 +14,56 @@ class Person:
 	'''
 	__thisCount = 0;
 
-	def __init__(self, name):  
+	def __init__(self, name, age):  
 		self.name = name
+		self.age = age
 		self.birth = datetime.now()
-		print('He was a Person.')
+		print('It was a Person.')
 
-		print('\n'+name+' is created on '+ str(self.birth),'.\n')
+		print(name+' is created on '+ str(self.birth),'.\n')
 		Person.__thisCount += 1
-		
+
 	def thisFunctionName(self, name):
 		print(name, 'Called from class of Person.')
-		
+
 	def __del__(self):
-		print(self.name,'destroyed')
+		print(self.name,'destroyed.')
 		print('He was a Person.')
-		
-		
+
+	def __lt__(self, other):
+		return self.age < other.age
+		# Mahdi > Maryam
+
 class ItMan(Person, Test):  # In az ki bayad ers bari kone ? Az Person! # Hamamun esm darimo. Tarikh tavalod darimo ...
-	
+
 	def __init__(self, name, email):
 		self.name = name
 		self.email = email
-		print('He was an IT man :(.')
+		print('He was an IT man :(.\n')
+
 	def __del__(self):
 		print(self.name,'destroyed.')
 		print('He was an IT man :(.')
 
 if __name__ == '__main__':
-	
+
 	jadi = ItMan('Jadi', 'jadijadi@gmail.com')	
-	Mahdi = Person('Mahdi')
-	
-	print("issubclass(ItMan, Person):?" ,issubclass(ItMan, Person) )
-	print("isinstance(Mahdi, Person):?" ,isinstance(Mahdi, Person) )
-	print("Person.__count:",Mahdi._Person__thisCount)  # Inam az data hiding! ghablesh bayad __ bezarami! :-) Kheili mohem.
-	print("----------------------------------------------------------")
+
+	Mahdi = Person('Mahdi','24')
+	Maryam = Person('Sara','23')
+
+	print(1<3)
+	print(Mahdi.age < Maryam.age)
+
+	print()
+
+# New Functions
+
+# __gt__  >
+# __lt__  <
+# __eq__  ==
+# __ne__  !=
+# __ge__  >=
+# __le__  <=
+
+# 
