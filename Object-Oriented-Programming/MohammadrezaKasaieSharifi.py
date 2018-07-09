4bfb8b4d82d67c33e875e6879ae0cead9cfac389
Seyed Mahdi Kashani <mahdi.kashanipour@gmail.com>
2018-07-08 23:57:38 -0400

    constructor _init_(self)
    this is initialize ! (meghdardehi mikonim object ro)

--- Object-Oriented-Programming/MohammadrezaKasaieSharifi.py
+++ Object-Oriented-Programming/MohammadrezaKasaieSharifi.py
@@ -1,17 +1,15 @@
-class ClassName:  # Always ClassName! All start with Capital.
+class ClassName:
 	'''
 	author: Mahdi.
 	email: mahdi@uga.edu.
 	'''
+	# Different methods have different names due to their responsibilities. One of them is constructor which we make an object that method happens!
+	def __init__(self):
+		print('Object is created')
 	
-	def thisFunctionName(self, name):  # Always pass the self as an input.
+	
+	def thisFunctionName(self, name):
 		print(name)  # Har tabe besoorate pishfarz None ro bar migardoone.
 
 if __name__ == '__main__':  # Module besurate dasti ejra shode.
 	obj1 = ClassName()  # Making an instance from an object.
-	print(obj1.thisFunctionName('this is my name.'))  # Baraye function ee ke roye sheye object1 ejra mishe . mizarim.	
-
-
-# Note 2: har function ruye yek object ejra mishe!
-# Class avale har kalame bayad bozorg bashe! for function this is different:
-# thisFunctionName(self).
